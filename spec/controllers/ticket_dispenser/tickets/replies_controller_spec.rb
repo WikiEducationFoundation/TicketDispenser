027b42852c5a75be76ea484a260cd78ebe17567e
hardy1334 <hardychugh34@gmail.com>
2020-03-25 02:29:48 +0530

    spec added for replies controller

---
+++ spec/controllers/ticket_dispenser/tickets/replies_controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe TicketDispenser::Tickets::RepliesController, type: :request do
+      describe '#destroy' do
+            let!(:message) { create(:message) }
+            it 'should delete the specified note' do
+                  expect(TicketDispenser::Message.count).to eq(1)
+                  delete "/tickets/replies/#{message.id}"
+                  message_response = JSON.parse(response.body).with_indifferent_access
+                  expect(message_response[:id]).to eq(message.id)
+                  expect(TicketDispenser::Message.count).to eq(0)
+            end      
+      end
+end      
