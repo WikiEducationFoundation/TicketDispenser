8efa8860116d47a0fecd498a53fa445ed94ecffd
hardy1334 <hardychugh34@gmail.com>
2020-03-25 03:28:38 +0530

    indentation fix for replies controller spec

--- spec/controllers/ticket_dispenser/tickets/replies_controller_spec.rb
+++ spec/controllers/ticket_dispenser/tickets/replies_controller_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 describe TicketDispenser::Tickets::RepliesController, type: :request do
-      describe '#destroy' do
-            let!(:message) { create(:message) }
-            it 'should delete the specified note' do
-                  expect(TicketDispenser::Message.count).to eq(1)
-                  delete "/tickets/replies/#{message.id}"
-                  message_response = JSON.parse(response.body).with_indifferent_access
-                  expect(message_response[:id]).to eq(message.id)
-                  expect(TicketDispenser::Message.count).to eq(0)
-            end      
-      end
+  describe '#destroy' do
+    let!(:message) { create(:message) }
+    it 'should delete the specified note' do
+      expect(TicketDispenser::Message.count).to eq(1)
+      delete "/tickets/replies/#{message.id}"
+      message_response = JSON.parse(response.body).with_indifferent_access
+      expect(message_response[:id]).to eq(message.id)
+      expect(TicketDispenser::Message.count).to eq(0)
+     end      
+  end
 end      
